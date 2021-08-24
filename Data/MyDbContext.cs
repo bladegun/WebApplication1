1a3d09441873da1a8229e71a7a38d0e068f85107
Bladegun <Bladegun@BLADEGUN-ILLEGE>
2021-08-24 11:05:31 +0800

    Add project files.

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Table1> Table1s { get; set; }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.HasDefaultSchema("public");
+        }
+    }
+}
---
+++ Data/Table1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using NetTopologySuite.Geometries;
+
+namespace WebApplication1.Data
+{
+    [Table("table1")]
+    public class Table1
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public Point point { get; set; }
+    }
+}
